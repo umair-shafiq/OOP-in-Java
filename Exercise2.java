03a6ada77aa81b1cf096a73c4952d80fda87ab34
umair-shafiq <116309001+umair-shafiq@users.noreply.github.com>
2023-05-17 10:15:19 +0500

    Create Exercise2.java

---
+++ Exercise2.java
@@ -0,0 +1,39 @@
+public class Exercise2{
+
+  static class Dog{
+       private String name;
+       private String breed;
+
+       public Dog(String name, String breed){
+            this.name = name;
+            this.breed = breed;
+       }
+       
+       public void setName(String name){
+       	   this.name = name;
+       }
+
+       public void setbreed(String breed){
+       	   this.breed = breed;
+       }
+
+    }
+
+
+	public static void main(String[] args) {
+		Dog dog1 = new Dog("Buddy", "Golden Retriever");
+        System.out.println("Name: " + dog1.name + " And breed: " + dog1.breed);
+
+        Dog dog2 = new Dog("Charlie", "Bulldog");
+        System.out.println("Name: " + dog2.name + " And breed: " + dog2.breed);
+
+        dog1.setName("Puppy");
+        dog1.setbreed("Labrador Retriever");
+        dog2.setName("Daisy");
+        dog2.setbreed("Hidi");
+
+        System.out.println("********* Updated Values **************");
+        System.out.println("Name: " + dog1.name + " And breed: " + dog1.breed);
+        System.out.println("Name: " + dog2.name + " And breed: " + dog2.breed);
+	}
+}
